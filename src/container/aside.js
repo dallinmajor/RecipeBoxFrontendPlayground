4ae5692bb78353fc36da335d950dff5b290dfc09
Dally Wally <dallin.watac.major@gmail.com>
2019-02-04 21:27:56 -0700

    added layout files

---
+++ src/container/aside.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Aside extends Component {
+    render() {
+        return(
+            <nav>
+
+            </nav>
+        )
+    }
+}
+
+export default Aside;
---
+++ src/container/main.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Main extends Component {
+    render() {
+        return(
+            <nav>
+
+            </nav>
+        )
+    }
+}
+
+export default Main;
