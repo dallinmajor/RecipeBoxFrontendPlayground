409a0c8d8273db0f2f5554fddf0e458bfdcdd571
Dally Wally <dallin.watac.major@gmail.com>
2019-02-05 19:15:59 -0700

    adding more css

---
+++ src/components/brand.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div>
+            <h1>Recipe Box</h1>
+            <button class='toggle-btn'></button>
+        </div>
+    )
+}
--- src/container/nav.js
+++ src/container/nav.js
@@ -1,10 +1,20 @@
 import React, {Component} from 'react';
+import Brand from '../components/brand';
+import Toggle from '../components/toggle';
+import Search from '../components/forms/nav-search';
 
 class Nav extends Component {
     render() {
         return(
             <nav>
-                nav
+                <Brand/>
+                <Toggle>
+                    <ul>
+                        <li><Search/></li>
+                        <li>New Recipe</li>
+                        <li>Logout</li>
+                    </ul>
+                </Toggle>
             </nav>
         )
     }
